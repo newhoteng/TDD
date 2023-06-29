3ec5ef47e56452c4372cf834b18fdc3fd76db75b
Harriet Oteng <harriet.oteng@gmail.com>
2023-06-29 15:10:08 +0000

    Remove unused code

--- solver.rb
+++ solver.rb
@@ -13,22 +13,6 @@ class Solver
     word.reverse
   end
 
-  # def fizzbuzz(num)
-  #   string = ''
-  #   (1..num).each do |i|
-  #     string += if (i % 3).zero? && (i % 5).zero?
-  #                 'fizzbuzz'
-  #               elsif (i % 3).zero?
-  #                 'fizz'
-  #               elsif (i % 5).zero?
-  #                 'buzz'
-  #               else
-  #                 i.to_s
-  #               end
-  #   end
-  #   string
-  # end
-
   def fizzbuzz(num)
     if (num % 3).zero? && (num % 5).zero?
       'fizzbuzz'
